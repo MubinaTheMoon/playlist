0d2a97aadb9489b416ca7a5ddde055bd7acaa298
mubina <mubina@gmail.com>
2025-01-24 13:54:15 +0500

    edited

--- js/app.js
+++ js/app.js
@@ -1,29 +1,85 @@
 const audio = document.getElementById("audio");
 const playBtn = document.getElementById("play");
-const pauseBtn = document.getElementById("pause");
 const forwardBtn = document.getElementById("forward");
 const backwardBtn = document.getElementById("backward");
-const img = document.getElementById("img");
 const voice = document.getElementById("voice");
-const volume = document.getElementById("volume");
-const transformText = document.querySelector("#name");
+const audioVolume = document.getElementById("volume");
+const titleMusic = document.querySelector("#name");
+const container = document.getElementById("container");
+const cover = document.getElementById("cover");
+const processDuration = document.querySelector(".process-duration");
+// const musicPlaying = document.getElementById("music-playing");
+// const musicDuration = document.getElementById("music-duration");
 
+let indexContent = 0;
+const content = [
+  "TheFatRat - Mayday",
+  "TheFatRat - Escaping Gravity (feat. Cecilia Gault)",
+  "TheFatRat - The Calling",
+  "Баста - Я Найду Тебя Через Века",
+  "Loreen - Tattoo",
+];
+
+function changeMusic(index) {
+  cover.src = `../img/${content[index]}.jpg`;
+  audio.src = `../music/${content[index]}.mp3`;
+  titleMusic.textContent = content[index];
+}
+changeMusic(indexContent);
+
+audio.volume = voice.value / 100;
 voice.addEventListener("input", () => {
   audio.volume = voice.value / 100;
-  volume.innerHTML = `${audio.volume * 100}`;
+  audioVolume.textContent = audio.volume;
 });
+
+function play() {
+  audio.play();
+  container.classList.add("playing");
+  playBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+}
+function pause() {
+  audio.pause();
+  container.classList.remove("playing");
+  playBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+}
 
 playBtn.addEventListener("click", () => {
-  audio.play();
-  img.classList.add("rotate");
-  pauseBtn.style.display = "inline-block";
-  playBtn.style.display = "none";
-  transformText.classList.add("run");
+  const isPlaying = container.classList.contains("playing");
+
+  if (isPlaying) {
+    pause();
+  } else {
+    play();
+  }
 });
 
-pauseBtn.addEventListener("click", () => {
-  audio.pause();
-  img.classList.remove("rotate");
-  pauseBtn.style.display = "none";
-  playBtn.style.display = "inline-block";
-});
+function nextMusic() {
+  if (content.length - 1 <= indexContent) {
+    indexContent = 0;
+  } else {
+    indexContent++;
+  }
+  changeMusic(indexContent);
+  play();
+}
+
+function prevMusic() {
+  if (content.length - 2 <= indexContent) {
+    indexContent = 0;
+  } else {
+    indexContent--;
+  }
+  changeMusic(indexContent);
+  play();
+}
+
+function durationMusic() {
+  let duration = audio.duration;
+  let currentTime = audio.currentTime;
+  processDuration.style.width = `${(currentTime / duration) * 100}%`;
+}
+forwardBtn.addEventListener("click", nextMusic);
+backwardBtn.addEventListener("click", prevMusic);
+audio.addEventListener("ended", nextMusic);
+audio.addEventListener("timeupdate", durationMusic);
